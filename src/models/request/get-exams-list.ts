29868e6d94bb621fcdb1d3a55fc0ad096ec2efe6
Ashay Gupta <ashaygupta10@gmail.com>
2017-06-25 02:14:51 +0530

    get exam list modal created

---
+++ src/models/request/get-exams-list.ts
@@ -0,0 +1,14 @@
+export class GetExamsList{
+  jobId: string;
+  jobTitle: string;
+  jobDesc: string;
+  examStartDate: string;
+  examEndDate: string;
+  seoKey: string;
+  seoDesc: string;
+  uploadDate: string;
+  pageUrl: string;
+  creatorId: string;
+  userDTO: string[];
+  active: string;
+}
--- src/pages/sarkari-results/sarkari-results.ts
+++ src/pages/sarkari-results/sarkari-results.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { ServiceProviders } from '../../providers/service-providers';
+import { GetExamsList } from '../../models/request/get-exams-list';
 
 @Component({
   selector: 'page-sarkari-results',
@@ -10,8 +11,9 @@ import { ServiceProviders } from '../../providers/service-providers';
 
 export class SarkariResults {
 
-  resultsNames: Array<string>;
-  resultsList: any[];
+  private resultsNames: Array<string>;
+  private resultsList: any[];
+  private getExamsList : GetExamsList;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public service: ServiceProviders) {
     // this.resultsNames = [' Senior School Certificate Examination (Class XII ) 2017 Announced on 28th May 2017', 'Sports', 'Computer', 'Science', 'History', 'Awards and Honours', 'Geography', 'Constitution', 'SSC', 'Banks', 'Politics', 'Economy'];
@@ -23,8 +25,11 @@ export class SarkariResults {
     //     increment: i + 1
     //   });
     // }
+    this.getExamsList = new GetExamsList();
 
     service.getSarkariResultData().then(data => {
+
+
       
     });
   }
