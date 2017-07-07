17743b97e91c20e7f7ad55a62f12578531c07e3b
Ashay Gupta <ashaygupta10@gmail.com>
2017-07-08 01:44:54 +0530

    increment

--- src/pages/sarkari-results/sarkari-results.ts
+++ src/pages/sarkari-results/sarkari-results.ts
@@ -16,7 +16,7 @@ export class SarkariResults {
   // startDates : Array<string>;
   // endDates : Array<string>;
   // examsList: any[];
-  private getResultsList : GetResultsList[];
+  private getResultsList : GetResultsList[]=[];
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public service: ServiceProviders, public util: Util) {
     // this.examNames = ['NVS Recruitment Navodaya Vidyalaya Samiti 351 PGTs TGTs and Faculty cum System Administration Vacancy','Sports','Computer','Science','History','Awards and Honours','Geography','Constitution','SSC','Banks','Politics','Economy'];
@@ -24,15 +24,6 @@ export class SarkariResults {
     // this.startDates = ['15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017'];
 
     // this.endDates = ['15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017'];
-
-    // this.examsList = [];
-    // for(let i=0; i < this.examNames.length; i++){
-    //   this.examsList.push({
-    //     name: this.examNames[i],
-    //     startDate: this.startDates[i],
-    //     endDate: this.endDates[i]
-    //   });
-    // }
 
     this.getResultsListData();
   }
