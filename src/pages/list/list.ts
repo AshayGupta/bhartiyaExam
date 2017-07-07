987077e7675c93bb90d812748b1be6fe37dee746
Ashay Gupta <ashaygupta10@gmail.com>
2017-07-08 00:56:57 +0530

    naukri page

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { ServiceProviders } from '../../providers/service-providers';
+import { GetExamsList } from '../../models/request/get-exams-list';
 
 @Component({
   selector: 'page-list',
@@ -9,28 +11,39 @@ import { NavController, NavParams } from 'ionic-angular';
 
 export class ListPage {
 
-  examNames : Array<string>;
-  startDates : Array<string>;
-  endDates : Array<string>;
-  examsList: any[];
+  // examNames : Array<string>;
+  // startDates : Array<string>;
+  // endDates : Array<string>;
+  // examsList: any[];
+  private getExamsList : GetExamsList[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.examNames = ['NVS Recruitment Navodaya Vidyalaya Samiti 351 PGTs TGTs and Faculty cum System Administration Vacancy','Sports','Computer','Science','History','Awards and Honours','Geography','Constitution','SSC','Banks','Politics','Economy'];
+  constructor(public navCtrl: NavController, public navParams: NavParams, public service: ServiceProviders) {
+    // this.examNames = ['NVS Recruitment Navodaya Vidyalaya Samiti 351 PGTs TGTs and Faculty cum System Administration Vacancy','Sports','Computer','Science','History','Awards and Honours','Geography','Constitution','SSC','Banks','Politics','Economy'];
+    //
+    // this.startDates = ['15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017'];
+    //
+    // this.endDates = ['15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017'];
+    //
+    // this.examsList = [];
+    // for(let i=0; i < this.examNames.length; i++){
+    //   this.examsList.push({
+    //     name: this.examNames[i],
+    //     startDate: this.startDates[i],
+    //     endDate: this.endDates[i]
+    //   });
+    // }
 
-    this.startDates = ['15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017'];
-
-    this.endDates = ['15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017'];
-
-    this.examsList = [];
-    for(let i=0; i < this.examNames.length; i++){
-      this.examsList.push({
-        name: this.examNames[i],
-        startDate: this.startDates[i],
-        endDate: this.endDates[i]
-      });
+      this.getExamsListData();
     }
 
-  }
+
+    private getExamsListData(){
+    this.service.getSarkariExamsList().then((data: GetExamsList[]) => {
+      this.getExamsList = data;
+    }, (error) => {
+          console.log("ERROR: ", error);
+      });
+    }
 
   itemTapped(item) {
 
--- src/providers/service-providers.ts
+++ src/providers/service-providers.ts
@@ -1,25 +1,38 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
-
+import { Constant } from '../utils/constant';
 
 @Injectable()
 export class ServiceProviders {
+
+  private examsListUrl = Constant.appBaseUrl + 'examsList';
+  private resultsListUrl = Constant.appBaseUrl + 'resultsList';
 
   constructor(public http: Http) {
 
   }
 
-  public getSarkariResultData(){
+  public getSarkariExamsList(){
     return new Promise(resolve => {
-      this.http.get('http://www.bhartiyaexam.com/examsList','').map(response => response.json()).subscribe(data => {
-        console.log("Sarkari result data", data);
+      this.http.get(this.examsListUrl,'').map(response => response.json()).subscribe(data => {
+        console.log("Sarkari exams list", data);
         resolve(data);
       }, (error) => {
-          console.log("Error from getSarkariResultData");
+          console.log("Error from getSarkariExamsList");
       });
     });
+  }
 
+  public getSarkariResultsList(){
+    return new Promise(resolve => {
+      this.http.get(this.resultsListUrl,'').map(response => response.json()).subscribe(data => {
+        console.log("Sarkari results list", data);
+        resolve(data);
+      }, (error) => {
+          console.log("Error from getSarkariResultsList");
+      });
+    });
   }
 
 }
