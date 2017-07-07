217f68a63c22c62d12519720bc3441eb24d91e9a
Ashay Gupta <ashaygupta10@gmail.com>
2017-07-08 00:57:11 +0530

    result page

--- src/pages/sarkari-results/sarkari-results.ts
+++ src/pages/sarkari-results/sarkari-results.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { ServiceProviders } from '../../providers/service-providers';
-import { GetExamsList } from '../../models/request/get-exams-list';
+import { GetResultsList } from '../../models/request/get-results-list';
 
 @Component({
   selector: 'page-sarkari-results',
@@ -11,34 +11,41 @@ import { GetExamsList } from '../../models/request/get-exams-list';
 
 export class SarkariResults {
 
-  private resultsNames: Array<string>;
-  private resultsList: any[];
-  private getExamsList : GetExamsList;
+  // examNames : Array<string>;
+  // startDates : Array<string>;
+  // endDates : Array<string>;
+  // examsList: any[];
+  private getResultsList : GetResultsList[];
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public service: ServiceProviders) {
-    // this.resultsNames = [' Senior School Certificate Examination (Class XII ) 2017 Announced on 28th May 2017', 'Sports', 'Computer', 'Science', 'History', 'Awards and Honours', 'Geography', 'Constitution', 'SSC', 'Banks', 'Politics', 'Economy'];
-    //
-    // this.resultsList = [];
-    // for (let i = 0; i < this.resultsNames.length; i++) {
-    //   this.resultsList.push({
-    //     name: this.resultsNames[i],
-    //     increment: i + 1
+    // this.examNames = ['NVS Recruitment Navodaya Vidyalaya Samiti 351 PGTs TGTs and Faculty cum System Administration Vacancy','Sports','Computer','Science','History','Awards and Honours','Geography','Constitution','SSC','Banks','Politics','Economy'];
+
+    // this.startDates = ['15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017'];
+
+    // this.endDates = ['15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017','15.06.2017'];
+
+    // this.examsList = [];
+    // for(let i=0; i < this.examNames.length; i++){
+    //   this.examsList.push({
+    //     name: this.examNames[i],
+    //     startDate: this.startDates[i],
+    //     endDate: this.endDates[i]
     //   });
     // }
-    this.getExamsList = new GetExamsList();
 
-    service.getSarkariResultData().then(data => {
-
-
-      
-    });
+    this.getResultsListData();
   }
 
 
-
+  private getResultsListData(){
+    this.service.getSarkariResultsList().then((data: GetResultsList[]) => {
+      this.getResultsList = data;
+    }, (error) => {
+          console.log("ERROR: ", error);
+      });
+  }
 
   itemTapped(item) {
 
   }
-
 }
